c5391622a4400dcedfcd15da87826a9de033052e
Alberto Hideyo Noguchi Filho <alberto2611@gmail.com>
2023-11-10 22:48:22 +0000

    refactor: melhorando codigo `tabby`

--- tabby/js/main.js
+++ tabby/js/main.js
@@ -1,17 +1,16 @@
-const tabs = Array.from(document.querySelectorAll(".tab"));
 const tabby = document.querySelector(".tabby");
+const tabsList = tabby.querySelector(".tabs");
+const tabs = Array.from(tabby.querySelectorAll(".tab"));
 const tabContents = Array.from(tabby.querySelectorAll(".tab-content"));
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (event) => {
-    // Finding the correct tab content
-    const target = tab.dataset.theme;
-    const tabContent = tabby.querySelector("#" + target);
-    // Selecting a tab
-    tabs.forEach((t) => t.classList.remove("is-selected"));
-    tab.classList.add("is-selected");
-    // Selecting a tab-content
-    tabContents.forEach((c) => c.classList.remove("is-selected"));
-    tabContent.classList.add("is-selected");
-  });
+tabsList.addEventListener("click", (event) => {
+  const tab = event.target;
+  const target = tab.dataset.theme;
+  const tabContent = tabby.querySelector("#" + target);
+  // Selects a tab
+  tabs.forEach((t) => t.classList.remove("is-selected"));
+  tab.classList.add("is-selected");
+  // Selects the corresponding tab content
+  tabContents.forEach((c) => c.classList.remove("is-selected"));
+  tabContent.classList.add("is-selected");
 });
