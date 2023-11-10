6618f19074efd1899955b56f4f683330d7aba5ca
Alberto Hideyo Noguchi Filho <alberto2611@gmail.com>
2023-11-10 22:32:40 +0000

    refactor: melhorando codigo `modal`

--- modal/js/main.js
+++ modal/js/main.js
@@ -1,5 +1,6 @@
 const modalButton = document.querySelector(".jsModalButton");
 const modalCloseButton = document.querySelector(".jsModalClose");
+const modalOverlay = document.querySelector(".modal-overlay");
 
 modalButton.addEventListener("click", (event) => {
   document.body.classList.add("modal-is-open");
@@ -7,4 +8,11 @@ modalButton.addEventListener("click", (event) => {
 
 modalCloseButton.addEventListener("click", (event) => {
   document.body.classList.remove("modal-is-open");
+});
+
+modalOverlay.addEventListener("click", (event) => {
+  if (!event.target.closest(".modal")) {
+    // Close modal
+    document.body.classList.remove("modal-is-open");
+  }
 });
